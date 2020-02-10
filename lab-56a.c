94faa23e8e1fe9fa13bb00b83df287b903972712
Ruslebiff <joakivb@stud.ntnu.no>
2020-02-10 23:25:08 +0100

    lag-56a ferdig

--- lab-56a.c
+++ lab-56a.c
@@ -21,7 +21,7 @@ struct threadargs {
 void* tfunc(void *arg) {
   int i;
   struct threadargs *targs=arg;
-  sem_wait(&sem[targs->id-1]);
+  sem_wait(&sem[targs->id]);
   printf("Tråd %d kjører\n", targs->id);
   sleep(targs->sec);
   printf("Tråd %d er ferdig og vekker kanskje andre...\n", targs->id);
@@ -44,27 +44,46 @@ int main(void)
     for(j=0;j<6;j++) { targs[i]->signal[j]=0; }
   }
 
-  targs[0]->id=1;             /* thread number 1 */
+  targs[0]->id=0;             /* thread number */
   targs[0]->sec=1;            /* how long to sleep */
   targs[0]->signal[1]=1;      /* which threads to wake up when done */
   targs[0]->signal[4]=1;
-  sem_init(&sem[targs[0]->id-1],SHARED,1); /* start waiting or running? */
+  sem_init(&sem[targs[0]->id],SHARED,1); /* start waiting or running? */
   pthread_create(&tid[0], NULL, tfunc, (void *) targs[0]);
 
-// GJENTA KODELINJENE OVER FOR DE FEM NESTE TRÅDENE, BARE ENDRE PARAMETRE
+  targs[1]->id=1;             /* thread number */
+  targs[1]->sec=2;            /* how long to sleep */
+  targs[1]->signal[3]=1;      /* which threads to wake up when done */
+  sem_init(&sem[targs[1]->id],SHARED,0); /* start waiting or running? */
+  pthread_create(&tid[1], NULL, tfunc, (void *) targs[1]);
 
-  for(i=0;i<1;i++) {
+  targs[2]->id=2;             /* thread number */
+  targs[2]->sec=3;            /* how long to sleep */
+  sem_init(&sem[targs[2]->id],SHARED,1); /* start waiting or running? */
+  pthread_create(&tid[2], NULL, tfunc, (void *) targs[2]);
+
+  targs[3]->id=3;             /* thread number */
+  targs[3]->sec=2;            /* how long to sleep */
+  sem_init(&sem[targs[3]->id],SHARED,0); /* start waiting or running? */
+  pthread_create(&tid[3], NULL, tfunc, (void *) targs[3]);
+
+  targs[4]->id=4;             /* thread number */
+  targs[4]->sec=3;            /* how long to sleep */
+  targs[4]->signal[5]=1;      /* which threads to wake up when done */
+  sem_init(&sem[targs[4]->id],SHARED,0); /* start waiting or running? */
+  pthread_create(&tid[4], NULL, tfunc, (void *) targs[4]);
+
+  targs[5]->id=5;             /* thread number */
+  targs[5]->sec=3;            /* how long to sleep */
+  sem_init(&sem[targs[5]->id],SHARED,0); /* start waiting or running? */
+  pthread_create(&tid[5], NULL, tfunc, (void *) targs[5]);
+
+
+
+  for(i=0;i<6;i++) {
     pthread_join(tid[i], NULL);
   }
 
   return 0;
 }
-
-
-
-
-
-
-
-
 
